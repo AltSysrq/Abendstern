f0095f1c19e49bc46d18568d00a989cd098e45b1
Jason Lingle <jasonlingle@yahoo.com>
2012-02-01 16:08:46 -0700

    Add the ConnectionListener class.

---
+++ src/net/connection_listener.cxx
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * @author Jason Lingle
+ * @date 2012.02.01
+ * @brief Implementation of ConnectionListener class
+ */
+
+#include <cstring>
+#include <algorithm>
+#include <string>
+
+#include "connection_listener.hxx"
+#include "antenna.hxx"
+
+using namespace std;
+
+static const byte applicationName[] = "Abendstern";
+//Just assume the hash is zero for now
+static const byte protocolHash[16] = {0};
+
+static const byte header[] = {
+  0, 0, //seq
+  0, 0, //chan
+  2, //STX
+};
+
+void ConnectionListener::process(const Antenna::endpoint& source,
+                                 Antenna* antenna,
+                                 Tuner* tuner,
+                                 const byte* data,
+                                 unsigned len)
+noth {
+  static const byte reply_wrongApplication[] =
+    "\0\0" //Seq
+    "\0\0" //Chan
+    "\4" //EOT
+    "Wrong application: Abendstern\0" //Error message
+    "wrong_application" //l10n entry, implict \0 at end
+    ;
+  static const byte reply_protocolMismatch[] =
+    "\0\0" //Seq
+    "\0\0" //Chan
+    "\4" //EOT
+    "Protocol mismatch\0"
+    "protocol_mismatch"
+    ;
+
+  //Packet length must be exactly:
+  //  header + hash + application
+  //long, and the application names must match exactly
+  if (len != sizeof(header)+sizeof(protocolHash)+sizeof(applicationName)
+  ||  0 != memcmp(data+sizeof(header)+sizeof(protocolHash),
+                  applicationName, sizeof(applicationName))) {
+    antenna->send(source, reply_wrongApplication,
+                  sizeof(reply_wrongApplication));
+    return;
+  }
+
+  //Verify compatible protocols
+  if (memcmp(data+sizeof(header), protocolHash, sizeof(protocolHash))) {
+    antenna->send(source, reply_protocolMismatch,
+                  sizeof(reply_protocolMismatch));
+    return;
+  }
+
+  //OK
+  string errmsg("Connection refused"), errl10n("connection_refused");
+  if (!acceptConnection(source, antenna, tuner, errmsg, errl10n)) {
+    //Refusing connection
+    //Header is 5 bytes, and there is one extra byte for each term nul.
+    vector<byte> reply(7+errmsg.size()+errl10n.size(), 0);
+    reply[4] = 4; //EOT
+    copy(errmsg.begin(), errmsg.end(), reply.begin()+5);
+    copy(errl10n.begin(), errl10n.end(), reply.begin()+6+errmsg.size());
+    antenna->send(source, &reply[0], reply.size());
+  }
+}
---
+++ src/net/connection_listener.hxx
@@ -0,0 +1,55 @@
+/**
+ * @file
+ * @author Jason Lingle
+ * @date 2012.02.01
+ * @brief Contains the ConnectionListener packet processing class.
+ */
+#ifndef CONNECTION_LISTENER_HXX_
+#define CONNECTION_LISTENER_HXX_
+
+#include <string>
+
+#include "packet_processor.hxx"
+
+/**
+ * The ConnectionListener class is a PacketProcessor that
+ * detects and responds to incomming connection-openning
+ * packets.
+ *
+ * A "connection-openning" packet is a packet meeting the
+ * definition of a standard communication packet whose
+ * sequence and channel numbers are both zero, followed
+ * by a SCG type of STX.
+ *
+ * If the network protocol hash and application name match what is expected,
+ * an abstract function is called to possibly open the connection. Otherwise,
+ * an EOT with an appropriate error message is sent.
+ */
+class ConnectionListener: public PacketProcessor {
+public:
+  virtual void process(const Antenna::endpoint& source,
+                       Antenna* antenna, Tuner* tuner,
+                       const byte* data, unsigned len) noth;
+
+protected:
+  /**
+   * Called when a valid, compatible connection request is received.
+   * @param source the endpoint representing the remote peer
+   * @param antenna the antenna performing the communications
+   * @param tuner the tuner used to filter the communications
+   * @param errmsg if an error occurs, use this error message, which
+   * is an English description of the error; initialised to
+   * "Connection Refused".
+   * @param errl10n if an error occurs, use this l10n entry for the error;
+   * initialised to "connection_refused"
+   * @return true if the connection is accepted; false if an error occurs.
+   * Note that on returning true, this class takes no further action ---
+   * it is up to the subclass to associate the endpoint with a connection, etc.
+   */
+  virtual bool acceptConnection(const Antenna::endpoint& source,
+                                Antenna* antenna, Tuner* tuner,
+                                std::string& errmsg,
+                                std::string& errl10n) noth = 0;
+};
+
+#endif /* CONNECTION_LISTENER_HXX_ */
