920249d7d355be7b74df00164e10afd032c97958
Jason Lingle <jasonlingle@yahoo.com>
2012-02-06 15:33:46 -0700

    Add the GameAdvertiser class.

---
+++ src/net/game_advertiser.cxx
@@ -0,0 +1,67 @@
+/**
+ * @file
+ * @author Jason Lingle
+ * @date 2012.02.06
+ * @brief Implementation of the GameAdvertiser class
+ */
+
+#include <cstring>
+#include <iostream>
+
+#include "game_advertiser.hxx"
+#include "antenna.hxx"
+#include "tuner.hxx"
+#include "io.hxx"
+
+using namespace std;
+
+static const byte trigger[] = {'A', 'b', 'e', 'n', 'd', 's', 'u', 'c', 'h'};
+
+GameAdvertiser::GameAdvertiser(Tuner* tuner,
+                               bool v6_,
+                               Uint32 overseerid_,
+                               byte peerCount_,
+                               bool passwordProtected_,
+                               const char* gameMode_)
+: v6(v6_),
+  overseerid(overseerid_),
+  peerCount(peerCount_),
+  passwordProtected(passwordProtected_)
+{
+  setGameMode(gameMode_);
+  tuner->trigger(trigger, sizeof(trigger), this);
+}
+
+void GameAdvertiser::setOverseerId(Uint32 oid) {
+  overseerid = oid;
+}
+
+void GameAdvertiser::setPeerCount(byte cnt) {
+  peerCount = cnt;
+}
+
+void GameAdvertiser::setGameMode(const char* mode) {
+  strncpy(gameMode, mode, 4);
+}
+
+void GameAdvertiser::process(const Antenna::endpoint& source,
+                             Antenna* antenna, Tuner* tuner,
+                             const byte* data, unsigned len)
+noth {
+  //Ignore if it is the wrong protocol
+  if (v6 == (source.protocol() == asio::ip::udp::v6())) {
+    //OK
+    byte pack[] = {
+      'A', 'b', 'e', 'n', 'd', 's', 'p', 'i', 'e', 'l',
+      overseerid >> 24, overseerid >> 16, overseerid >> 8, overseerid >> 0,
+      peerCount, passwordProtected,
+      gameMode[0], gameMode[1], gameMode[2], gameMode[3],
+    };
+    try {
+      antenna->send(source, pack, sizeof(pack));
+    } catch (asio::system_error e) {
+      cerr << "Warning: Unable to respond to Abendsuch from " << source
+           << ": " << e.what() << endl;
+    }
+  }
+}
---
+++ src/net/game_advertiser.hxx
@@ -0,0 +1,60 @@
+/**
+ * @file
+ * @author Jason Lingle
+ * @date 2012.02.06
+ * @brief Contains the GameAdvertiser packet processing class
+ */
+#ifndef GAME_ADVERTISER_HXX_
+#define GAME_ADVERTISER_HXX_
+
+#include <SDL.h>
+
+#include "packet_processor.hxx"
+
+/**
+ * Listens for "Abendsuch" packets from unknown hosts and replies with
+ * Abendstern game information.
+ *
+ * The class must be configured for EITHER IPv4 or IPv6, since all games
+ * must use the same protocol for all peers.
+ */
+class GameAdvertiser: public PacketProcessor {
+  bool v6;
+  Uint32 overseerid;
+  byte peerCount, passwordProtected;
+  char gameMode[4];
+
+public:
+  /**
+   * Creates and registers the GameAdvertiser.
+   * @param tuner the Tuner to register with
+   * @param v6 if true, use IPv6; otherwise, use IPv4
+   * @param overseerid the current peer ID of the overseeh
+   * @param peerCount the current number of peers in the game
+   * @param passwordProtected whether a password is required to join the game
+   * @param gameMode a string describing the game mode; the first four
+   * characters are used if it is longer than 4.
+   */
+  GameAdvertiser(Tuner* tuner, bool v6,
+                 Uint32 overseerid, byte peerCount,
+                 bool passwordProtected, const char* gameMode);
+  /**
+   * Alters the overseer peer ID to the given value.
+   */
+  void setOverseerId(Uint32);
+  /**
+   * Alters the peer count to the given value.
+   */
+  void setPeerCount(byte);
+  /**
+   * Alters the game mode to the given string. Only the first four characters
+   * are used.
+   */
+  void setGameMode(const char*);
+
+  virtual void process(const Antenna::endpoint& source,
+                       Antenna* antenna, Tuner* tuner,
+                       const byte* data, unsigned len) noth;
+};
+
+#endif /* GAME_ADVERTISER_HXX_ */
