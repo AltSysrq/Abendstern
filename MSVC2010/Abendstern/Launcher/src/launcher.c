6abe95b09b586377b8cc12c6f9062e5db75cc090
Jason Lingle <jasonlingle@yahoo.com>
2012-02-17 23:24:59 +0000

    Add Launcher project.

---
+++ MSVC2010/Abendstern/Launcher/src/launcher.c
@@ -0,0 +1,50 @@
+#ifndef WINVER
+#define WINVER 0x0510
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT WINVER
+#endif
+
+#ifndef _WIN32_WINDOWS
+#define _WIN32_WINDOWS WINVER
+#endif
+
+//#ifndef _WIN32_IE                       // Specifies that the minimum required platform is Internet Explorer 7.0.
+//#define _WIN32_IE 0x0700        // Change this to the appropriate value to target other versions of IE.
+//#endif
+
+#include <windows.h>
+#include <SDL.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+  char cmdline[4096];
+  char exe[] = "bin\\abw32gl14.exe";
+  int i;
+  STARTUPINFOA sinfo = {
+    sizeof(STARTUPINFO),
+    0 //Init rest with zeros as well
+  };
+  PROCESS_INFORMATION info;
+  FILE* truncator;
+  // Truncate the log file before starting
+  truncator = fopen("log.txt", "w");
+  if (truncator) fclose(truncator);
+  truncator = fopen("launchlog.txt", "w");
+  if (truncator) fclose(truncator);
+  strcpy(cmdline, "bin\\abw32gl14.exe");
+  strcat(cmdline, " ");
+  for (i=1; i<argc; ++i) {
+    strcat(cmdline, argv[i]);
+    strcat(cmdline, " ");
+  }
+  printf("cmdline: %s\n", cmdline);
+  CreateProcessA(exe, cmdline, NULL, NULL, FALSE, CREATE_BREAKAWAY_FROM_JOB, NULL, NULL, &sinfo, &info);
+  CloseHandle(info.hProcess);
+  CloseHandle(info.hThread);
+  return 0;
+}
