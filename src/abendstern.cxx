aecee140cd6203ffc769eca23d737fcab55e87bf
Jason Lingle <jasonlingle@yahoo.com>
2012-05-16 10:00:27 -0600

    Actually save new abendstern.rc under home directory.

--- src/abendstern.cxx
+++ src/abendstern.cxx
@@ -465,9 +465,10 @@ bool init() {
         cerr << "Error reading configuration: " << e.what() << endl;
         return false;
       }
-      //Save in the new location
-      conf.renameFile("conf", configFileLocation);
     }
+    //Save in the new location
+    conf.renameFile("conf", configFileLocation);
+    conf.sync("conf");
   }
   try {
     conf.open("shaders/stacks.rc", "shaders");
