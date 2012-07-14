d7cc889cbee88ef1e75dde0ccdffb9dabeaa9d01
Jason Lingle <jasonlingle@yahoo.com>
2012-07-14 13:59:20 -0600

    Multiply font height by vheight to adapt to 16:9 etc.

--- src/core/init_state.cxx
+++ src/core/init_state.cxx
@@ -220,7 +220,7 @@ float InitState::systexLoader() {
 }
 
 float InitState::initFontLoader() {
-  float mult = (preliminaryRunMode? 2.0f : 1.0f);
+  float mult = (preliminaryRunMode? 2.0f : vheight);
   float size = conf["conf"]["hud"]["font_size"];
   new (sysfont)         Font("fonts/westm",   size*mult, false);
   new (sysfontStipple)  Font("fonts/westm",   size*mult, true );
