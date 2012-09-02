5fb05a274b37acb47d9ebbd9a1009eee8c4f7174
Jason Lingle <jasonlingle@yahoo.com>
2012-09-02 14:04:33 -0600

    Frame recorder is now always present, but disabled except via Tcl.

--- src/secondary/frame_recorder.hxx
+++ src/secondary/frame_recorder.hxx
@@ -14,13 +14,6 @@
 #ifndef FRAME_RECORDER_HXX_
 #define FRAME_RECORDER_HXX_
 
-/** If #defined, the frame recorder is enabled.
- * Don't define this in production!
- */
-#define FRAME_RECORDER_ENABLED
-
-#ifdef FRAME_RECORDER_ENABLED
-
 /** Contains an interface to Abendstern's built-in
  * frame recorder. The recorder, when active, captures the current
  * screen at a framerate of exactly 30 FPS and stores them to
@@ -31,15 +24,16 @@
  * It assumes that the directory "recorder" already exists.
  * The images may be converted to a video with the command
  * &nbsp;  <code>ffmpeg -r 30 -b 1200 -i %08d.bmp out.mpg</code>
- *
- * THIS SHOULD NEVER BE ENABLED IN A PRODUCTION BUILD.
  */
 namespace frame_recorder {
   /** Sets internal variables up. This MUST be called
    * before any of the other functions.
    */
   void init();
-  /** Begins a new capture run. */
+  /** Begins a new capture run.
+   *
+   * Does nothing if enable() hasn't been called.
+   */
   void begin();
   /** Ends the current capture run. */
   void end();
@@ -47,8 +41,18 @@ namespace frame_recorder {
   bool on();
   /** Updates the frame recorder. */
   void update(float);
-}
 
-#endif /* FRAME_RECORDER_ENABLED */
+  /** Enables usage of the frame recorder.
+   *
+   * This must be called before any of the functions herein (other than init())
+   * do anything.
+   */
+  void enable();
+
+  /**
+   * Sets the framerate for recording. By default, the rate is 30 FPS.
+   */
+  void setFrameRate(float);
+}
 
 #endif /* FRAME_RECORDER_HXX_ */
